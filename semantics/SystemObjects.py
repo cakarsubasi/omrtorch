cdb670cc7aecdedc0e2bbe099f0cd493c9d42dd3
CEM AKARSUBASI <CAKARSUBASI18@ku.edu.tr>
2022-04-16 17:09:03 +0300

    Created SongFactory class

--- semantics/SystemObjects.py
+++ semantics/SystemObjects.py
@@ -1,5 +1,7 @@
 from typing import Optional, Tuple
+import torch
 import numpy as np
+
 
 class Staff():
     '''
@@ -14,12 +16,12 @@ class Staff():
     # Constant to decide whether to include or reject a measure
     TOLERANCE = 0.01
 
-    def __init__(self, measureObj: np.array=None):
+    def __init__(self, measureObj: np.array = None):
         self.measures = []
         self.stats = {}
         if measureObj is not None:
             self.measures.append(measureObj)
-    
+
     def append(self, measureObj: np.array) -> bool:
         if len(self.measures) == 0:
             self.measures.append(measureObj)
@@ -38,10 +40,10 @@ class Staff():
         o_center = o.getStats()['center']
 
         return self_center < o_center
-    
+
     def __str__(self):
         return str(self.getStats())
-        
+
     def getStats(self):
         self._calculateStats()
         return self.stats
@@ -56,7 +58,7 @@ class Staff():
 
         lines = np.linspace(top, bottom, 5)
         lines = np.hstack([lines[0], np.repeat(lines[1:4], 2), lines[4]])
-        lines = np.reshape(lines, (4,2))
+        lines = np.reshape(lines, (4, 2))
         return lines
 
     def getStaffLinesBBs(self) -> np.array:
@@ -71,35 +73,41 @@ class Staff():
     def _calculateStats(self):
         measures = np.array(self.measures)
 
-        self.stats['top'] = np.average(measures[:,1])
-        self.stats['bottom'] = np.average(measures[:,3])
-        self.stats['left'] = np.min(measures[:,0])
-        self.stats['right'] = np.max(measures[:,2])
-        self.stats['center'] = self.stats['top'] + (self.stats['bottom'] - self.stats['top'])/2
+        self.stats['top'] = np.average(measures[:, 1])
+        self.stats['bottom'] = np.average(measures[:, 3])
+        self.stats['left'] = np.min(measures[:, 0])
+        self.stats['right'] = np.max(measures[:, 2])
+        self.stats['center'] = self.stats['top'] + \
+            (self.stats['bottom'] - self.stats['top'])/2
         self.stats['num'] = measures.shape[0]
+
 
 class SystemStaff():
     '''
     Contain one or two staffs
     '''
-    def __init__ (self, staff1: Staff, staff2: Optional[Staff]=None):
+
+    def __init__(self, staff1: Staff, staff2: Optional[Staff] = None):
         self.staff1 = staff1
-        
+
         if staff2 is not None:
             self.staff2 = staff2
-        
-    
+
     pass
+
 
 class Measure():
     '''
     key: key signature
     '''
 
+
 class Song():
     '''
-    Get a tuple of system measures
+    Contains system staffs which contain one or more staff objects
+    Keeps track of staff regions and assigns notes to these regions
     '''
+
     def __init__(self, systems: Tuple[SystemStaff], image: np.array, notes: np.array):
         self.systems = systems
         self.image = image
@@ -108,21 +116,105 @@ class Song():
     def assignNotes(self):
         pass
 
-    pass
-
-class Page():
-    '''
-    Factory for songs
-    '''
-    def __init__(self, image, measuredetections, objectdetections):
+    def toStream(self):
         '''
-        :param image:
+        Generate a music21 stream from object'''
 
-        
-        '''
-        self.image = image
-        self.measuredetections = measuredetections
-        self.objectdetections = objectdetections
         pass
 
     pass
+
+
+class SongFactory():
+    '''
+    Factory for songs
+    '''
+
+    MEASURE_THRESHOLD = 0.75
+
+    def __init__(self, image, measuredetections, objectdetections):
+
+        self.image = image
+        self.height, self.width = image.shape[1:3]
+        self.system_measures = None
+        self.staff_measures = None
+        self.staves = None
+        self.boundaries = None
+
+        # filter unreliable results
+        best_boxes = torch.where(
+            measuredetections['scores'] > self.MEASURE_THRESHOLD)
+
+        detections_reduced = {}
+        detections_reduced['boxes'] = measuredetections['boxes'][best_boxes]
+        detections_reduced['labels'] = measuredetections['labels'][best_boxes]
+        detections_reduced['scores'] = measuredetections['scores'][best_boxes]
+
+        # get system measures and staff measures
+        system_measures = detections_reduced['boxes'][torch.where(
+            detections_reduced['labels'] == 1)]
+        staff_measures = detections_reduced['boxes'][torch.where(
+            detections_reduced['labels'] == 2)]
+
+        # move tensors to CPU
+        system_measures = system_measures.cpu().detach().numpy()
+        staff_measures = staff_measures.cpu().detach().numpy()
+
+        # normalize
+        staff_measures[:, [1, 3]] = staff_measures[:, [1, 3]]/self.height
+        staff_measures[:, [0, 2]] = staff_measures[:, [0, 2]]/self.width
+
+        system_measures[:, [1, 3]] = system_measures[:, [1, 3]]/self.height
+        system_measures[:, [0, 2]] = system_measures[:, [0, 2]]/self.width
+
+        # sort by y axis (ascending)
+        sort_order = np.argsort(staff_measures[:, 1])
+        staff_measures = staff_measures[sort_order]
+
+        sort_order = np.argsort(system_measures[:, 1])
+        system_measures = system_measures[sort_order]
+
+        self.system_measures = system_measures
+        self.staff_measures = staff_measures
+
+        # Set up staffs
+        staves = []
+        staves.append(Staff())
+        for i in range(staff_measures.shape[0]):
+            if staves[-1].append(staff_measures[i]) is True:
+                pass
+            else:
+                nextstaff = Staff()
+                staves.append(nextstaff)
+                staves[-1].append(staff_measures[i])
+
+        self.staves = staves
+
+        # get staff boundaries
+        boundaries = get_staff_boundaries(
+            [staff.stats['center'] for staff in self.staves])
+        self.boundaries = boundaries
+
+        # TODO: system detection
+
+        # TODO: process object detections
+        self.objectdetections = objectdetections
+        pass
+
+    def visualize(self):
+        # TODO
+        pass
+
+
+def get_staff_boundaries(measure_centers):
+    '''
+    returns an Nx2 ndarray 
+    '''
+    x1 = measure_centers
+    x2 = np.roll(measure_centers, 1)
+    gaps = ((x1+x2)/2)[1:]
+
+    x3 = np.concatenate([[0], gaps])
+    x4 = np.concatenate([gaps, [1]])
+
+    return np.stack([x3, x4], axis=1)
